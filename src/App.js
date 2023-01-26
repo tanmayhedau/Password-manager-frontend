b7555f54923c9c9826565003bd88863db17daf8d
tanmayhedau <tanmayhedau619@gmail.com>
2023-01-26 20:51:13 +0530

    final push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [passwordList, setPasswordList] = useState([]);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/showpasswords").then((response) => {
+      setPasswordList(response.data);
+    });
+  },[]);
+
+  const addPassword = () => {
+    Axios.post("http://localhost:3001/addpassword", {
+      password: password,
+      title: title,
+    });
+  };
+
+  const decryptPassword = (encryption) => {
+    Axios.post("http://localhost:3001/decryptpassword", {
+      password: encryption.password,
+      iv: encryption.iv,
+    }).then((response) => {
+      setPasswordList(
+        passwordList.map((val) => {
+          return val.id === encryption.id
+            ? {
+                id: val.id,
+                password: val.password,
+                title: response.data,
+                iv: val.iv,
+              }
+            : val;
+        })
+      );
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AddingPassword">
+        <input
+          type="text"
+          placeholder="Ex.password123"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <input
+          type="text"
+          placeholder="Ex.Facebook"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        ></input>
+        <button onClick={addPassword}>Add Password</button>
+      </div>
+
+      <div className="Passwords">
+        {passwordList.map((val, key) => {
+          return (
+            <div
+              className="password"
+              onClick={() => {
+                decryptPassword({
+                  password: val.password,
+                  iv: val.iv,
+                  id: val.id,
+                });
+              }}
+              key={key}
+            >
+              <h3>{val.title}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
